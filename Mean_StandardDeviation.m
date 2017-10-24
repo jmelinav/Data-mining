9d16db3acf4447b851d5577a87e3aa35a5eb2bc4
mdepak <m.deepak@ymail.com>
2017-10-24 16:21:54 -0700

    Used z-normalization on mean

--- Mean_StandardDeviation.m
+++ Mean_StandardDeviation.m
@@ -1,5 +1,5 @@
-eating_file = 'Vinoth/spoon/EatingAction/EA.csv';
-non_eating_file = 'Vinoth/spoon/NonEatingAction/NEA.csv'
+eating_file = 'Data/EatingAction/EA.csv';
+non_eating_file = 'Data/NonEatingAction/NEA.csv'
 
 [ea_mean_mat, ea_std_mat] = applyMeanAndStandardDeviation(eating_file);
 [nea_mean_mat, nea_std_mat] = applyMeanAndStandardDeviation(non_eating_file);
@@ -7,16 +7,32 @@ non_eating_file = 'Vinoth/spoon/NonEatingAction/NEA.csv'
 %Plot the data of the mean of the features
 for idx = 1:18
     name =  sprintf('Mean of Feature %d', idx);
-    plot([transpose(ea_mean_mat(:,idx)); transpose(nea_mean_mat(:,idx))].');
+    %plot([transpose(ea_mean_mat(:,idx)); transpose(nea_mean_mat(:,idx))].');
+    %title(name)
+    h1 = plot(1:2569, transpose(ea_mean_mat(1:2569, idx))', 'b');     %first_y has 4 columns so h1 is length 4
+    hold on
+    h2 = plot(1:2569, transpose(nea_mean_mat(:, idx)), 'r');    %second_y has 3 columns so h2 is length 3
+    legend('Eating Action', 'Non eating action')
+    
     title(name)
     pause;
-end
-
+    hold off
+    pause;
+ end
+    
 %Plot the data of the standard deviation of the features
 for idx = 1:18
     name =  sprintf('Standard Deviation of Feature %d', idx);
-    plot([transpose(ea_std_mat(:,idx)); transpose(nea_std_mat(:,idx))].');
+    %plot([transpose(ea_std_mat(1:2569,idx)); transpose(nea_std_mat(1:2569,idx))].');
+    %title(name)
+    h1 = plot(1:2569, transpose(ea_std_mat(1:2569, idx))', 'b');     %first_y has 4 columns so h1 is length 4
+    hold on
+    h2 = plot(1:2569, transpose(nea_std_mat(1:2569, idx)), 'r');    %second_y has 3 columns so h2 is length 3
+    legend('Eating Action', 'Non eating action')
+    
     title(name)
+    pause;
+    hold off
     pause;
 end
 
@@ -29,15 +45,17 @@ thisLine = fgetl(fid);
 
 mean_matrix = [];
 std_matrix = [];
+count = 0
 
 while ischar(thisLine)
     Z = textscan(thisLine,'%f','Delimiter',',')';
+    count = count +1;
     thisLine= fgetl(fid);
-    mean_matrix = [mean_matrix; mean(cell2mat(Z))];
-    std_matrix = [std_matrix; std(cell2mat(Z))];
+    mean_matrix = [mean_matrix; mean(zscore(cell2mat(Z)))];
+    std_matrix = [std_matrix; std(zscore(cell2mat(Z)))];
 end
 
 fclose(fid);
-result_mean_matrix = reshape(mean_matrix, [40,18]);
-result_std_matrix = reshape(std_matrix, [40,18]);
+result_mean_matrix = reshape(mean_matrix, [count/18,18]);
+result_std_matrix = reshape(std_matrix, [count/18,18]);
 end
